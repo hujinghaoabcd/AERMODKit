45241c095140542749c501e5b2db397156a00232
Jinghao Hu <731409213@qq.com>
2026-07-29 02:19:29 +0800

    Normalize version module imports

--- src/aermodkit/spec/model_version.py
+++ src/aermodkit/spec/model_version.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
 import re
+from dataclasses import dataclass
 
 _VERSION_PATTERN = re.compile(r"^(?:v)?(?P<code>\d{5})$", re.IGNORECASE)
 
